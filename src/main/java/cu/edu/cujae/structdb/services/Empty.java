218527b105909eeb5c800d670010e7faa86cfdf3
Carlos Daniel Robaina Rivero <cdrobayna01@gmail.com>
2023-03-08 21:04:16 -0500

    init: :one: Created project structure

---
+++ src/main/java/cu/edu/cujae/structdb/services/Empty.java
@@ -0,0 +1,8 @@
+package cu.edu.cujae.structdb.services;
+
+public class Empty {
+    /*
+     * This file is only for maintaining the project structure in GitHub
+     * Please delete this file after other class is created in this package.
+     * */
+}
